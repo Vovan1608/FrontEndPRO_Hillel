bd550bc058cd653dac4d6c7f163da91cc394c4e7
Vovan1608 <vovan1608@gmail.com>
2020-09-17 11:58:54 +0300

    solved 1.2

--- #1_Intro/1.2/main.js
+++ #1_Intro/1.2/main.js
@@ -1,11 +1,15 @@
-﻿var firsAge = 18;
-var secondAge = 16;
-var thirdAge = 19;
+﻿var first = "18 лет Юрий Иванов";
+var second = "16 лет Дмитрий Кузьмин";
+var third = "19 лет Оксана Петрова";
 
 console.log("**************");
 console.log("Список студентов зарегестрировавшихся на вебинар:"); 
-console.log("1. " + firsAge + " лет Юрий Иванов"); 
-console.log("2. " + secondAge + " лет Дмитрий Кузьмин");
-console.log("3. " + thirdAge + " лет Оксана Петрова"); 
-console.log("------------------- Средний возраст студента: " + parseInt((firsAge + secondAge + thirdAge) / 3) + " --------------");
+console.log("1. " + first); 
+console.log("2. " + second);
+console.log("3. " + third); 
+console.log(
+    "------------------- Средний возраст студента: " + 
+    (parseInt(first) + parseInt(second) + parseInt(third)) / 3 + 
+    " --------------"
+);
 console.log("**************");
