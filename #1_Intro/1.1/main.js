c6b5c93b233c121c4105bbc94c95b3d45975b320
Vovan1608 <vovan1608@gmail.com>
2020-09-17 10:41:19 +0300

    solved 1.1

--- #1_Intro/1.1/main.js
+++ #1_Intro/1.1/main.js
@@ -1,1 +1,12 @@
-﻿
+﻿var force = 125;
+var weight = 5;
+var acceleration = force / weight;
+
+document.write(
+	'**************</br>' + 
+	'Ускорение тела при силе F = ' + [force] + 
+	' и массе m = ' + [weight] + 
+	' :</br>--------------------</br>' +
+	' a = ' + [acceleration] + 
+	' .</br>-------------------</br>end.'
+);
