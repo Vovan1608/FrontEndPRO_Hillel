4beb7070ebf61d1a5db790be783c59036e32f55a
Vovan1608 <vovan1608@gmail.com>
2020-09-17 11:36:13 +0300

    3 variables

--- #1_Intro/1.2/main.js
+++ #1_Intro/1.2/main.js
@@ -1,12 +1,11 @@
 ﻿var firsAge = 18;
 var secondAge = 16;
 var thirdAge = 19;
-var middleAge = parseInt((firsAge + secondAge + thirdAge) / 3);
 
 console.log("**************");
 console.log("Список студентов зарегестрировавшихся на вебинар:"); 
 console.log("1. " + firsAge + " лет Юрий Иванов"); 
 console.log("2. " + secondAge + " лет Дмитрий Кузьмин");
 console.log("3. " + thirdAge + " лет Оксана Петрова"); 
-console.log("------------------- Средний возраст студента: " + middleAge + " --------------");
+console.log("------------------- Средний возраст студента: " + parseInt((firsAge + secondAge + thirdAge) / 3) + " --------------");
 console.log("**************");
