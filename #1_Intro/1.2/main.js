fa70ac940c76b70f1872a0a42147f9ce0df38190
Vovan1608 <vovan1608@gmail.com>
2020-09-17 11:27:58 +0300

    solved 1.2

---
+++ #1_Intro/1.2/main.js
@@ -0,0 +1,12 @@
+﻿var firsAge = 18;
+var secondAge = 16;
+var thirdAge = 19;
+var middleAge = (firsAge + secondAge + thirdAge) / 3;
+
+console.log("**************");
+console.log("Список студентов зарегестрировавшихся на вебинар:"); 
+console.log("1. " + firsAge + " лет Юрий Иванов"); 
+console.log("2. " + secondAge + " лет Дмитрий Кузьмин");
+console.log("3. " + thirdAge + " лет Оксана Петрова"); 
+console.log("------------------- Средний возраст студента: " + parseInt(middleAge) + " --------------")
+console.log("**************");
