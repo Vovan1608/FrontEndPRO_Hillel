278e86e853d69726c8f9db6f446229afebe7cf80
Vovan1608 <vovan1608@gmail.com>
2020-09-22 12:59:54 +0300

    solved 2.8

---
+++ #2_if_else_loops/2.8/main.js
@@ -0,0 +1,19 @@
+﻿/*
+Вывести n чисел из ряда Фибоначчи, где n - вводится с клавиатуры
+*/
+
+var num = +prompt("Enter number","");
+
+var start = 1;
+var second;
+
+for( var i = 0; i < num; i++){
+	if(i < 2){
+		second = start;
+	}else{
+		var fibonachi = start + second;
+		start = second;
+		second = fibonachi;
+	}
+	console.log(second);
+}
