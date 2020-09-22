c7f8817218cdd1b7a5c63312896f4f6f4f62decf
Vovan1608 <vovan1608@gmail.com>
2020-09-22 11:17:11 +0300

    solved 2.6

---
+++ #2_if_else_loops/2.6/main.js
@@ -0,0 +1,18 @@
+﻿/*
+Ввести с клавиатуры 2 числа a и b (где a < b). 
+Запустить цикл перебора от a до b. Вывести в 
+консоль квадраты чётных чисел.
+*/
+
+var a = +prompt("Enter a", "");
+var b = +prompt("Enter b > a","");
+
+if (a > b){
+	console.log("Incorrect.");
+}
+
+for (var i = a; i <= b; i++){
+	if(i % 2 == 0){
+		console.log( i * i);
+	}
+}
