7eede361f746fdfb133b50b9090a691ec4c084e5
Vovan1608 <vovan1608@gmail.com>
2020-09-24 22:26:04 +0300

    осталось массив сделать числом в 3.6

--- #3_Array/3.6/main.js
+++ #3_Array/3.6/main.js
@@ -8,7 +8,21 @@ var arrComponents = [];
 
 var divider = 10;
 
-while( (num / divider) > 0) {
-    numComponents = num % divider;
+for (var i = 0; (num / divider) > 0; i++){
+    arrComponents[i] = num % divider;
     num = parseInt( num / divider );
 }
+
+var arrSize = arrComponents.length;
+
+for(var i = arrSize - 1; i >= 1; i--){
+    for(var j = 0; j < i; j++){
+        if(arrComponents[j] > arrComponents[j + 1]){
+            var temp = arrComponents[j];
+            arrComponents[j] = arrComponents[j + 1];
+            arrComponents[j + 1] = temp;
+        }
+    }
+}
+
+console.log(arrComponents);
