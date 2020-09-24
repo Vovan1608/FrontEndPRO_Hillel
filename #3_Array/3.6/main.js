a2e68183269b580a5ac81f517a64c6cb2bf5d59c
Vovan1608 <vovan1608@gmail.com>
2020-09-24 14:57:47 +0300

    added 3.6

---
+++ #3_Array/3.6/main.js
@@ -0,0 +1,14 @@
+﻿/*
+Вывести в порядке возрастания цифры, из которых состоит число 
+например: 95824 - в консоли должен увидеть 2 4 5 8 9
+*/
+
+var num = 95824;
+var arrComponents = [];
+
+var divider = 10;
+
+while( (num / divider) > 0) {
+    numComponents = num % divider;
+    num = parseInt( num / divider );
+}
