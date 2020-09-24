e07063dbf267f96cb699710d124f2431b8c20ac3
Vovan1608 <vovan1608@gmail.com>
2020-09-24 07:14:46 +0300

    added 3.1

---
+++ #3_Array/3.1/main.js
@@ -0,0 +1,17 @@
+﻿/*
+Заполнить массив А случайными числами (диазпазон на ваше усмотрение). 
+Заменить каждый элемент массива с нечетным индексом на ноль.
+*/
+
+var a = [];
+a.length = Math.round(Math.random() * 10);
+
+for (var i = 0, aSize = a.length; i < aSize; i++){
+    if( (i % 2) != 0 ){
+        a[i] = 0; 
+    }else{
+        a[i] = Math.round(Math.random() * 100);
+    }
+}
+
+console.log(a);
