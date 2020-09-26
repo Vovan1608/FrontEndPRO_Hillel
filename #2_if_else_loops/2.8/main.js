325e8ac5b509db5e01883fecdcda2d4c8b80a4fa
Vovan1608 <vovan1608@gmail.com>
2020-09-26 15:19:13 +0300

    check

--- #2_if_else_loops/2.8/main.js
+++ #2_if_else_loops/2.8/main.js
@@ -2,12 +2,14 @@
 Вывести n чисел из ряда Фибоначчи, где n - вводится с клавиатуры
 */
 
-var num = +prompt("Enter number","");
+do{
+	var number = prompt("Введите число", "");
+}while ( isNaN(number) || number == null || number.trim() === "" || number <= 0);
 
 var start = 1;
 var second;
 
-for( var i = 0; i < num; i++){
+for( var i = 0; i < number; i++){
 	if(i < 2){
 		second = start;
 	}else{
