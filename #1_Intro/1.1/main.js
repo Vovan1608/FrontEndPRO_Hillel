f92936f8db763a7a8e236912bcea998f37a480c0
Vovan1608 <vovan1608@gmail.com>
2020-09-26 13:58:43 +0300

    с помощью интерполяции 1.1

--- #1_Intro/1.1/main.js
+++ #1_Intro/1.1/main.js
@@ -3,10 +3,12 @@ var weight = 5;
 var acceleration = force / weight;
 
 document.write(
-	'**************</br>' + 
-	'Ускорение тела при силе F = ' + force + 
-	' и массе m = ' + weight + 
-	' :</br>--------------------</br>' +
-	' a = ' + acceleration + 
-	' .</br>-------------------</br>end.'
+	`
+	**************</br>
+	Ускорение тела при силе F = ${force} и массе m = ${weight}
+	:</br>--------------------</br> 
+	a = ${acceleration}. 
+	</br>-------------------</br>
+	end.
+	`
 );
