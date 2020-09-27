08d6a26b80e08286aac958b167ab4a2798db2955
Vovan1608 <vovan1608@gmail.com>
2020-09-27 12:34:09 +0300

    little change

--- #3_Array/3.7/main.js
+++ #3_Array/3.7/main.js
@@ -12,20 +12,20 @@ console.log([1, 2, 3, 4, 5].join("_"));
 //2)
 let arr = [6, 7, 2, 3, 5, 7];
 
-document.write(`<ul>`);
+document.write("<ul>");
 for(let i = 0, arrSize = arr.length; i < arrSize; i++){
     document.write(`<li>${arr[i]}</li>`);
 }
-document.write(`</ul>`);
+document.write("</ul>");
 
 
-document.write(`<br>---------------------`);
+document.write("<br>---------------------");
 
 document.write("<ul><li>");
 document.write(arr.join("</li><li>"));
 document.write("</li></ul>")
 
-document.write(`<br>---------------------<br>`);
+document.write("<br>---------------------<br>");
 
 
 //3)
