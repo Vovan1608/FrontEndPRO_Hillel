0244401d1affbfe6600ed8716d7481ab34ae0574
Vovan1608 <vovan1608@gmail.com>
2020-09-28 10:44:10 +0300

    used ternary operator

--- #4_Array_2/4.1/main.js
+++ #4_Array_2/4.1/main.js
@@ -7,15 +7,9 @@
 Если массив непарный - последний элемент не трогать
 */
 
-let arr = [1, 2, 3, 4, 5, 6];
+let arr = [1, 2, 3, 4, 5, 6, 7];
 
-let arrSize;
-
-if(arr.length % 2 == 0){
-    arrSize = arr.length;
-}else{
-    arrSize = arr.length - 1;
-}
+let arrSize = (arr.length % 2 == 0) ? arr.length : arr.length - 1;
 
 for(let i = 0; i < arrSize; i += 2){
     [ arr[i], arr[i + 1] ] = [ arr[i + 1], arr[i] ];
