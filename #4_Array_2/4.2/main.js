de2935bd21be0dc9e2c7984cad9033f43d7c9d0e
Vovan1608 <vovan1608@gmail.com>
2020-09-28 15:04:49 +0300

    added 4.2

---
+++ #4_Array_2/4.2/main.js
@@ -0,0 +1,29 @@
+﻿'use strict'
+
+/*
+Создать двумерный массив, заполненный случайными числами в любом диапазоне.
+1.Найти колонку, где сумма элементов наименьшая.
+2.Найти колонку, где сумма элементов найбольшая.
+*/
+
+let arr = [];
+
+arr.length = Math.round(Math.random() * 4);
+
+let sumColumn = 0;
+
+for (let i = 0, arrSize = arr.length; i < arrSize; i++){
+    
+    arr[i] = new Array(arrSize);
+    
+    for(let j = 0; j < arrSize; j++){
+        arr[i][j] = Math.round(Math.random() * 10);
+    }
+
+    sumColumn += arr[i][0];
+
+}
+
+console.log(arr);
+console.log(sumColumn)
+
