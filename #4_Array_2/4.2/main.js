d98a155da080480d69664fef9e5d98a3017d9ef8
Vovan1608 <vovan1608@gmail.com>
2020-09-29 10:59:32 +0300

    solved 4.2

--- #4_Array_2/4.2/main.js
+++ #4_Array_2/4.2/main.js
@@ -27,5 +27,10 @@ for (let i = 0, arrSize = arr.length; i < arrSize; i++){
 }
 
 console.log(arr);
-
 console.log(sumColumn);
+
+let min = sumColumn.indexOf(Math.min(...sumColumn));
+let max = sumColumn.indexOf(Math.max(...sumColumn));
+
+console.log(`Наименьшая сумма элементов находится в колонке ${min + 1}`);
+console.log(`Наибольшая сумма элементов находится в колонке ${max + 1}`);
