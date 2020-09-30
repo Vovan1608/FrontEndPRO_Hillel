395854c9e1058790fdff2e53777ae0b4bbc6e67e
Vovan1608 <vovan1608@gmail.com>
2020-09-30 23:20:42 +0300

    added 5.1

---
+++ #5_Function/5.1/main.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+/*
+Написать функцию заполнения массива. Имя произвольное.
+*/
+
+function isNumber(value){
+    if(value <= 0 || isNaN(value) || value === null || value === ""){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function fillingArray(arrSize){
+    
+    if(isNumber(arrSize)){
+        return "arrSize is not a correct";
+    }else{
+        let arr = new Array (arrSize);
+    
+        for(let i = 0; i < arrSize; i++){
+            arr[i] = prompt("Enter data.", "");
+        }
+        return arr;
+    }
+}
+
+console.log(fillingArray(2));
