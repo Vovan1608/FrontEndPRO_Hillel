922845e4dd7c113926b7198785ee9be7cebd838e
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-01 18:44:29 +0300

    4 loops

--- #4_Array_2/4.2/main.js
+++ #4_Array_2/4.2/main.js
@@ -1,4 +1,4 @@
-﻿'use strict'
+﻿// 'use strict'
 
 /*
 Создать двумерный массив, заполненный случайными числами в любом диапазоне.
@@ -6,27 +6,49 @@
 2.Найти колонку, где сумма элементов найбольшая.
 */
 
-let arr = [];
+var arr = [];
 arr.length = 3;
-let sum = 0;
-let min = 0;
-let max = 0;
+var sumArr = [];
 
-for (let i = 0, arrSize = arr.length; i < arrSize; i++){
+
+for (var i = 0, arrSize = arr.length; i < arrSize; i++){
     
     arr[i] = new Array(arrSize);
     
-    for(let j = 0; j < arrSize; j++){
+    for(var j = 0; j < arrSize; j++){
         
-        arr[i][j] = Math.round(Math.random() * 10);
-        
-        if(j === 0){
-            sum += arr[i][j];
-        }
+        arr[i][j] = Math.round(Math.random() * 50);
     }
-
-    
 }
 
+var min = 0;
+var max = 0;
+var minI = 0;
+var maxI = 0;
+
+for(var i = 0; i < arr.length; i++){
+    
+    for(var j = 0, sum = 0; j < arr.length; j++){
+        sum += arr[j][i];
+    }
+    if(i === 0){
+        min = sum;
+        max = sum;
+    }
+
+    if(min > sum){
+        min = sum;
+        minI = i;
+    }
+
+    if(max < sum){
+        max = sum;
+        maxI = i;
+    }
+}
+
+
+
 console.log(arr);
-console.log(sum);
+console.log(`Наименьшая сумма єлементов равна ${min}  и находится в столбце ${minI + 1}`);
+console.log(`Наибольшая сумма єлементов равна ${max}  и находится в столбце ${maxI + 1}`);
