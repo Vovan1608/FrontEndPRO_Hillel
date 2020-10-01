57f43ef8255e6dbc31304fdbeb1d113011b399da
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-01 21:22:46 +0300

    solved 5.1

--- #5_Function/5.1/main.js
+++ #5_Function/5.1/main.js
@@ -4,26 +4,43 @@
 Написать функцию заполнения массива. Имя произвольное.
 */
 
-function isNumber(value){
-    if(value <= 0 || isNaN(value) || value === null || value === ""){
-        return true;
-    }else{
+function isCoorectArrLength(value){
+    if(value <= 0 || isNaN(value) || value === null || value === "" || typeof value === "undefined"){
         return false;
+    }else{
+        return true;
     }
 }
 
-function fillingArray(arrSize){
+function fillingArrey(arrSize){
     
-    if(isNumber(arrSize)){
-        return "arrSize is not a correct";
-    }else{
+    if( isCoorectArrLength(arrSize)){
         let arr = new Array (arrSize);
-    
+        
         for(let i = 0; i < arrSize; i++){
             arr[i] = prompt("Enter data.", "");
         }
         return arr;
+    }else{
+        return "arrSize is not a correct";
     }
 }
 
-console.log(fillingArray(2));
+let arr = fillingArrey(2);
+console.log(arr); // заполняем массив из двух элементов
+
+let dgh;
+console.log(fillingArrey(dgh));// arrSize is not a correct
+
+/*
+А вот если пробовать console.log( fillingArrey( dgh ) ) - ПАРАМЕТР без кавычек,
+то выдает ошибку Uncaught ReferenceError: dgh is not defined
+хотя проверка typeof value === "undefined" есть.
+
+И в консоли     typeof dgh === "undefined"
+                true
+
+Если пробовать
+let dgh;
+console.log(fillingArrey(dgh)), то проверка срабатывает;
+*/
