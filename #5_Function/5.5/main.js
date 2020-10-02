f4de7467690db2c6ebeda2c3c02b058f0d829d6d
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-02 10:21:26 +0300

    solve 5.5

--- #5_Function/5.5/main.js
+++ #5_Function/5.5/main.js
@@ -1,24 +1,27 @@
 ﻿"use strict";
 
 /*
-Создать функцию, которая убирает из строки все символы, 
-которые мы передали вторым аргументом.
+напиши функцию filter, которая принимает функцию-предикат и массив. 
+Возвращает она массив значений, для которых предикат вернет true.
 
-'func("hello world", ['l', 'd'])' вернет нам "heo wor"
+var input = [1, 2, 3, 4, 5, 6];
+function isEven(x) { return x % 2 == 0; } // проверяет на четность
+console.log(filter(input, isEven)); // [2, 4, 6]
 */
 
-function deleteValue(str, arr){
+function isEven(x) { return x % 2 == 0; }
 
-    let temp = str.split("");
-    for (let i = temp.length - 1 ; i > 0; i--){
-        for (let j = 0, arrSize = arr.length; j < arrSize; j++){
-            if(temp[i] === arr[j]){
-                temp.splice(i, 1);
-            }
+function filter(arr, check){
+
+    let input = [];
+
+    for(let i of arr){
+        if(isEven(i)){
+            input.push(i);
         }
     }
-    return temp.join("");
+    return input;
 }
 
-let d = deleteValue("hello world", ['l', 'd']); // вернет нам "heo wor"
-console.log(d)
+let input = [1, 2, 3, 4, 5, 6]; // [2, 4, 6]
+console.log(filter(input, isEven));
