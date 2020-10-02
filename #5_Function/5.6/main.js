f54c46cc08c9f15eb5cf3b67d87dd081bdf529b0
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-02 16:20:47 +0300

    added 5.6, 5.7

---
+++ #5_Function/5.6/main.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+/*
+Написать функицию которая вернет произведение переданных 
+фактических аргументов.
+*/
+
+function mul (){
+    
+    let res = 1;
+    
+    for(let i = 0, size = arguments.length; i < size; i++){
+        res *= arguments[i]; 
+    }
+
+    return res;
+}
+
+let res = mul (3, 4, 5, 1, 10);
+console.log(res);
---
+++ #5_Function/5.7/main.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+/*
+Функция должна вернуть массив из центральных элементов переданных массивов.
+
+Центральный элемент, это один центральный, если массив по длине 
+непарный [1, 2, 3, 4, 5] -> 3
+
+Центральный элемент, это пара центральных, если массив по длине 
+парный: [1, 2, 3, 4, 5, 6] -> 3, 4
+*/
+
+function getCentralElements (arrFirst, arrSecond) {
+    
+    var mass = [];
+
+    for(var i = 0; i < arguments.length; i++) {
+        var rem = getCutElems(arguments[i]);
+        mass = mass.concat(rem);
+    }
+    console.log(mass, 'mass');
+}
+
+
+function getCutElems(arr) {
+    
+    var count = arr.length % 2 == 0 ? 2 : 1;
+    var position = Math.ceil(arr.length / 2) - 1;
+    var rem = arr.splice(position, count);
+
+    return rem;
+} 
+
+getCentralElements([1, 2, 3, 5, 7], [6, 12, 3, 5, 7, 9, 4, 5], [6, 2, 9, 4, 5], [1, 2, 2, 3, 5, 8]); // [3, 5, 7, 9, 2, 3]
