e0853f94cb9813ba5181456f6847810c27bc80bb
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-03 09:48:47 +0300

    added isArry()

--- #5_Function/5.2/main.js
+++ #5_Function/5.2/main.js
@@ -8,8 +8,11 @@
 
 function checkArrs (arr1, arr2) {
 
-    return ( getSumElemArray(arr1) > getSumElemArray(arr2) ) ? arr1 : arr2;
-
+    if(isArray(arr1) && isArray(arr2)){
+        return ( getSumElemArray(arr1) > getSumElemArray(arr2) ) ? arr1 : arr2;
+    }else{
+        return "incorrect arr"
+    }
 }
 
 function isNumber(value){
@@ -22,14 +25,30 @@ function isNumber(value){
 
 function getSumElemArray(arr){
     
-    let sum = 0;
-    for (let i = 0, arrSize = arr.length; i < arrSize; i++){
-        
-        if( isNumber(arr[i]) ){
-            sum += arr[i]
+    // 1-st way
+    let sum = arr.reduce( (sum, next) => {
+        if( isNumber( next) ){
+            sum += next;
         }
-    }
+        return sum;
+    }, 0);
+
+    // 2-nd way
+    // for (let i = 0, arrSize = arr.length; i < arrSize; i++){
+        
+    //     if( isNumber(arr[i]) ){
+    //         sum += arr[i]
+    //     }
+    // }
     return sum;
+}
+
+function isArray(arr){
+    if(Array.isArray(arr)){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 let arr1 = [1, "1ghjk", 2, null, undefined, 3, 1];
@@ -37,6 +56,3 @@ let arr2 = [4, 5, "rty", 1, true, "", NaN, " "];
 
 let c = checkArrs(arr1, arr2);
 console.log(c); // [4, 5, "rty", 1, true, "", NaN, " "]
-
-console.log(getSumElemArray(arr1)); // 7
-console.log(getSumElemArray(arr2));// 10
