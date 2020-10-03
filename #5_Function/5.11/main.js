77187c3301706958329266fa063e665d68ff4ca0
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-03 09:26:28 +0300

    added 5.11

---
+++ #5_Function/5.11/main.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+
+/*
+Предусмотреть возможность передачи вторым аргументом функции. 
+При копировании массива - функция применится к каждому элементу 
+копируемого массива. 
+newL = copy(list, function(value){ return value*10; })
+*/
+
+function copyArr(arr, func){
+    
+    if(isArray(arr) && isFunction(func)){
+        return arr.map( (i) => mutationValue(i));
+    }else{
+        return "incorrect"
+    }
+    
+}
+
+let mutationValue = function (value) { 
+    return value * 10; 
+}
+
+function isArray(arr){
+    if(arr.constructor === Array){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function isFunction(callback){
+    if(typeof callback === "function"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let arr = [1, 2, 3];
+let a = copyArr(arr, mutationValue);
+console.log(a);
