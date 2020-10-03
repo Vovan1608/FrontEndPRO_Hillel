21c3608dfd7b2789c45de2d6a5bfc43aa24e9887
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-03 08:53:55 +0300

    solved 5.10

---
+++ #5_Function/5.10/main.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+/*
+Реализовать функцию copy(list) по копированию массива
+*/
+
+function copyArr(arr){
+    
+    if(isArray(arr)){
+        return arr.map( (index) => index);
+    }else{
+        return "arr is not arrey"
+    }
+    
+}
+
+function isArray(arr){
+    if(arr.constructor === Array){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let arr = [1, 2, 3];
+let a = copyArr(arr);
+console.log(a);
