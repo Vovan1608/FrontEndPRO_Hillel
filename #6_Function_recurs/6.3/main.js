1ccde685029fecdbac5efacf23d3415cdda672b1
Vovan1608 <vovan1608@gmail.com>
2020-10-04 19:48:25 +0300

    added 6.3

---
+++ #6_Function_recurs/6.3/main.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+/*
+Написать функция factorial(n) - которая при помощи 
+рекурсии будет считать значение факториала числа n!
+*/
+
+function getFactorial(n){
+    
+    return (n === 1) ? 1 : (n * getFactorial(n - 1));
+}
+
+console.log(getFactorial(5));
