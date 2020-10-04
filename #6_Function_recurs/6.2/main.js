5f17eea3a1d7b7971213db0ef0b3fec0510362a2
Vovan1608 <vovan1608@gmail.com>
2020-10-04 19:36:38 +0300

    added 6.2

---
+++ #6_Function_recurs/6.2/main.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+/*
+Написать рекурсивную функцию sumTo(n), которая для 
+данного n вычисляет сумму чисел от 1 до n, например:
+sumTo(1) = 1
+sumTo(2) = 2 + 1 = 3
+sumTo(3) = 3 + 2 + 1 = 6
+*/
+
+function sumTo(n){
+    
+    if(n === 1){
+        return 1;
+    }else{
+        return (n + sumTo(n - 1));
+    }
+}
+
+console.log(sumTo(3));
