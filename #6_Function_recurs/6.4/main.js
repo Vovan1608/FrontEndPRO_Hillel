96b88ccd86c5c651e98935d5b1edb79273335544
Vovan1608 <vovan1608@gmail.com>
2020-10-04 21:42:41 +0300

    added 6.4

---
+++ #6_Function_recurs/6.4/main.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+/*
+Cоздать конвертер-функцию которая получает на вход 
+массив вида: mass = [[1,2,3, [3.1]], 4, [5,6, [7, 8]]]; 
+а на выходе получим переобразованый 
+массив: [1,2,3,3.1,4,5,6,7,8]
+*/
+
+
+
+function mutationArray(arr){
+    
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            mutationArray(arr[i]);
+        }else{
+            
+            mutArray.push(arr[i]);
+            console.log(arr[i]);
+        }
+    }
+    return arr;
+}
+
+
+let mass = [[1,2,3, [3.1]], 4, [5,6, [7, 8]]];
+let mutArr = [];
+// mutationArray(mass)
+console.log(mutationArray(mass))
+// console.log(mutationArray(mass));
