b9d702af423b3617b6141d91bd786efca9db022a
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-08 13:21:43 +0300

    added 7.3

---
+++ #7_Object/7.3/main.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+/*
+Адаптировать функцию assignObjects() под неопределенное 
+количество объектов. assignObjects(obj1, obj2, ....., objn);
+*/
+
+function assignObjects(){
+    
+    let data = Object.assign({}, arguments[0]);
+    
+    for(let i = 1, size = arguments.length; i < size; i++){
+        Object.assign(data, arguments[i]); 
+    }
+    return data;
+}
+
+let res = assignObjects({ x: 10, y: 20 }, { z: 30 }, {s: 12, t: 25}); //-> { x: 10, y: 20, z: 30, s: 12, t: 25 }
+console.log(res);
+
+let res2 = assignObjects({ x: 10 }, { y: 20, z: 30 }, {y: 12, z: 25}); // - > { x: 10, y: 12, z: 25 }
+console.log(res2);
