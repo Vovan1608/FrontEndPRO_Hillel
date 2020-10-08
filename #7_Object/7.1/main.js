4d37a11bb39c6a3b02a9ac6149157e0319a36924
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-08 06:49:02 +0300

    added 7

---
+++ #7_Object/7.1/main.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+/*
+Вывести в консоль последовательность чисел в обратном порядке, 
+используя рекурсивный вызов функиции
+row (5) // 5 4 3 2 1
+*/
+
+function getPerson(names, from, to){
+    let person = {
+        name: names, 
+        age: Math.random() * (to - )}
+}
