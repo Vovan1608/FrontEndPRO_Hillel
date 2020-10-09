4f8f3314774ec42a84c0d4df384d702e5ddd672c
Vovan1608 <vovan1608@gmail.com>
2020-10-09 11:42:17 +0300

    code style

--- #7_Object/7.1/main.js
+++ #7_Object/7.1/main.js
@@ -8,14 +8,17 @@
 объект: newObj = { a: 10, b: 12, f: 13, m: 20 }
 */
 
-function convert(obj, newObj){
+function convert(obj, newObj) {
+    
     for(let key in obj){
-        if(typeof obj[key] === "object"){
+        
+        if(typeof obj[key] === "object") {
             convert(obj[key], newObj);
         }else{
             newObj[key] = obj[key];
         }
     }
+    
     return newObj;
 }
 
@@ -30,5 +33,6 @@ let any = {
     } 
 }
 let newObj = {};
+
 let res = convert(any, newObj);
 console.log(res);
