ebba5cc429dd7f26e8275d71a35610ee55b257ec
Vovan1608 <vovan1608@gmail.com>
2020-10-09 11:45:50 +0300

    code style

--- #7_Object/7.3/main.js
+++ #7_Object/7.3/main.js
@@ -5,12 +5,16 @@
 количество объектов. assignObjects(obj1, obj2, ....., objn);
 */
 
-function assignObjects(){
+function assignObjects() {
     // 1-й способ
-    for(let i = 1, size = arguments.length; i < size; i += 1){
-        for(let key in arguments[i])
-        arguments[0][key] = arguments[i][key];
+
+    for(let i = 1, size = arguments.length; i < size; i += 1) {
+        
+            for(let key in arguments[i]) {
+            arguments[0][key] = arguments[i][key];
+        }
     }
+
     return arguments[0];
     // 2-й способ
     // let data = Object.assign({}, arguments[0]);
@@ -22,5 +26,6 @@ function assignObjects(){
 
 let res = assignObjects({ x: 10, y: 20 }, { z: 30 }, {s: 12, t: 25}); //-> { x: 10, y: 20, z: 30, s: 12, t: 25 }
 console.log(res);
+
 let res2 = assignObjects({ x: 10 }, { y: 20, z: 30 }, {y: 12, z: 25}); // - > { x: 10, y: 12, z: 25 }
 console.log(res2);
