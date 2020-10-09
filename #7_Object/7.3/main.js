c2de4260bab6d7b39bf58709e2bc9a76861a8316
Vovan1608 <vovan1608@gmail.com>
2020-10-09 08:28:18 +0300

    code style, for...in

--- #7_Object/7.3/main.js
+++ #7_Object/7.3/main.js
@@ -6,17 +6,21 @@
 */
 
 function assignObjects(){
-    
-    let data = Object.assign({}, arguments[0]);
-    
-    for(let i = 1, size = arguments.length; i < size; i++){
-        Object.assign(data, arguments[i]); 
+    // 1-й способ
+    for(let i = 1, size = arguments.length; i < size; i += 1){
+        for(let key in arguments[i])
+        arguments[0][key] = arguments[i][key];
     }
-    return data;
+    return arguments[0];
+    // 2-й способ
+    // let data = Object.assign({}, arguments[0]);
+    // for(let i = 1, size = arguments.length; i < size; i++){
+    //     Object.assign(data, arguments[i]); 
+    // }
+    // return data;
 }
 
 let res = assignObjects({ x: 10, y: 20 }, { z: 30 }, {s: 12, t: 25}); //-> { x: 10, y: 20, z: 30, s: 12, t: 25 }
 console.log(res);
-
 let res2 = assignObjects({ x: 10 }, { y: 20, z: 30 }, {y: 12, z: 25}); // - > { x: 10, y: 12, z: 25 }
 console.log(res2);
