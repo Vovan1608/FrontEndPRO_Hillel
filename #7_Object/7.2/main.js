71a0ad4b65a296b99b9cdd42633288733defc373
Vovan1608 <vovan1608@gmail.com>
2020-10-09 08:17:20 +0300

    added for...in, code stile

--- #7_Object/7.2/main.js
+++ #7_Object/7.2/main.js
@@ -12,12 +12,16 @@ assignObjects({ x: 10 }, { x: 20, y: 30 }) - > { x:20, y: 30 }
 */
 
 function assignObjects(obj1, obj2){
-    
-    return Object.assign(obj1, obj2);
+    // 1-ый способ
+    for(let key in obj2){
+        obj1[key] = obj2[key];
+    }
+    return obj1;
+    // 2-ой способ
+    // return Object.assign(obj1, obj2);
 }
 
 let res = assignObjects({ x: 10, y: 20 }, { z: 30 }); //-> { x:10, y:20, z: 30 }
 console.log(res);
-
 let res2 = assignObjects({ x: 10 }, { x: 20, y: 30 }); // - > { x:20, y: 30 }
 console.log(res2);
