37231cc127b3078b1bd70b0cc2e225e2bfe966a4
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-10 15:39:30 +0300

    added (sumElemArr1 > sumElemArr2) ? sumElemArr1 : sumElemArr2;

--- #5_Function/5.2/main.js
+++ #5_Function/5.2/main.js
@@ -6,51 +6,50 @@
 большая - должен вернутся функцией.
 */
 
-function checkArrs (arr1, arr2) {
+// function checkArrs (arr1, arr2) {
 
-    // let arrForCheck =  getSumElemArray(arr1, arr2);
+//     // let arrForCheck =  getSumElemArray(arr1, arr2);
 
-    return ( getSumElemArray(arr1, arr2)[0] > getSumElemArray(arr1, arr2)[1] ) ? arr1 : arr2;
-}
+//     return ( getSumElemArray(arr1, arr2)[0] > getSumElemArray(arr1, arr2)[1] ) ? arr1 : arr2;
+// }
 
-function isNumber(value){
+function isNumber(value) {
+    
     if(isNaN(value) || value === "" || typeof value === "boolean" || typeof value === "string"){
         return false;
-    }else{
+    } else {
         return true;
     }
 }
 
-function getSumElemArray(arr1, arr2){
+function getSumElemArray(arr1, arr2) {
     
-    if(isArray(arr1) && isArray(arr2)){
+    if(isArray(arr1) && isArray(arr2)) {
         let arrSize = ( arr1.length > arr2.length ) ? arr1.length : arr2.length;
         
-        let sumElemArr1 = 0; 
-        let sumElemArr2 = 0;
+        let sumElemArr1 = 0, 
+            sumElemArr2 = 0;
         
         for (let i = 0; i < arrSize; i++){
             
-            if( isNumber(arr1[i]) ){
-                sumElemArr1 += arr1[i]
+            if( isNumber(arr1[i]) ) {
+                sumElemArr1 += arr1[i];
             }
 
-            if(isNumber(arr2[i])){
+            if(isNumber(arr2[i])) {
                 sumElemArr2 += arr2[i];
             }
         }
-        return [sumElemArr1, sumElemArr2];
+        return (sumElemArr1 > sumElemArr2) ? sumElemArr1 : sumElemArr2;
     }else{
-
         throw new Error("incorrect arr");
     }
-    
 }
 
-function isArray(arr){
-    if(Array.isArray(arr)){
+function isArray(arr) {
+    if(Array.isArray(arr)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -58,5 +57,5 @@ function isArray(arr){
 let arr1 = [1, "1ghjk", 2, null, undefined, 3, 1];
 let arr2 = [4, 5, "rty", 1, true, "", NaN, " "];
 
-let c = checkArrs(arr1, arr2);
+let c = getSumElemArray(arr1, arr2);
 console.log(c); // [4, 5, "rty", 1, true, "", NaN, " "]
