74849c18becddf0cd7723d8432a7f18bcf7be313
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-13 18:50:27 +0300

    added 9.3

---
+++ #9_Array.prototype && Array.prototype.reduce/9.3/main.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+/*
+Самостоятельно реализовать метод Array.prototype.splice 
+без использования стандартных метод Array.prototype 
+(допускаются методы push,pop, shift, unshift)
+
+Метод arr.splice(str) – Умеет всё: добавлять, удалять и заменять элементы.
+*/
+
+function splice(array) {
+    
+    return array;
+}
+
+function isArray(array) {
+    return Array.isArray(array);
+}
+
+function getIndexForSlice(array, index) {
+    index = (index > array.length) ? array.length : 
+            (index >= 0) ?  index : (array.length + index);
+    
+    return index;
+}
