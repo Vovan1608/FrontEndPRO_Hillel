85ff19149bc8adbb584ad365f7e8e2b2ff6e4720
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-13 12:51:43 +0300

    added 4.3

---
+++ #4_Array_2/4.3/main.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+/*
+В каждом объекте посчитать сумму свойств и перемножить значения объектов
+*/
+
+let obj = [
+    {a: 2, b: 2},        // a + b * ->
+    {a: 20, b: 20},      // a + b * ->
+    {a: 200, b: 200}     // a + b
+];
+
+let res = obj.reduce( (accum, next) => {
+    return accum * (next.a + next.b); 
+}, 1)
+
+console.log(res);
