82783d6234e1b862f1d3915b60d387dd4eb2a2bb
Vladymyr Zenin <vovan1608@gmail.com>
2020-10-17 16:11:44 +0300

    почти закончил

--- #9_Array.prototype && Array.prototype.reduce/9.3/main.js
+++ #9_Array.prototype && Array.prototype.reduce/9.3/main.js
@@ -19,45 +19,87 @@ function splice(array, start, deleteCount = 0) {
     let arrayDeletedVal = [],
         // arrayTempVal - значения, которые нужно сдвинуть 
         arrayTempVal = [],
-        originalArraySize = array.length,
+        // index с которого начинаются элементы для вставки в псевдомассиве arguments
+        startIncertElemInArg = 3,
+        // длина оригинального массива
+        originlArraySize = array.length,
+        // кол-во элементов которые нужно вставить
+        numOfIncertElem = arguments.length - startIncertElemInArg, 
         // кол-во итераций цикла
-        numberOfIterations = (array.length - start <= arguments.length - 3) ? arguments.length - 3 : array.length - start;
-        //  
+        numberOfIterations = (array.length - start <= numOfIncertElem) ? numOfIncertElem : array.length - start;
+        console.log(numberOfIterations, "iter")
+        // когда пройдут все итерации по заменам, потом цикл продолжится пока временный массив не станет пустым 
     for(let i = 0; i < numberOfIterations || Boolean(arrayTempVal.length) === true; i += 1) {
         
-        if(deleteCount < arguments.length - 3) {
-            // получаем два массива, важен второй - для значений, которые нужно сдвинуть
-            if(i < deleteCount) {
-                arrayDeletedVal[arrayDeletedVal.length] = array[i + start];
-                // меняем в нашем массиве значения
-                array[i + start] = arguments[i + 3];
-            } else {
-
-                if(array[i + start] !== undefined) {
+        if(i < numberOfIterations) {
+            // 1-ый вариант, когда удаляем 0 и впихиваем 1 и более
+            if(deleteCount === 0) {
+                // если элемент есть
+                if(array[i + start]) {
+                    // Элементы уходят в массив значений для сдвига
                     arrayTempVal[arrayTempVal.length] = array[i + start];
                 }
-                array[i + start] = arguments[i + 3];
-            }
-        } else {
-            // сразу удалять с конца элементы и распихивать их по массивам
-            
-            if(i < deleteCount) {
-                arrayDeletedVal[arrayDeletedVal.length] = array[i + start];
-                // меняем в нашем массиве значения
-                if(arguments[i + 3] !== undefined) {
-                    array[i + start] = arguments[i + 3];
+                //если есть элемент для вставки
+                if(arguments[i + startIncertElemInArg]) {
+                    // всовываем элемент 
+                    array[i + start] = arguments[i + startIncertElemInArg];
                 }
-            } else {
-                arrayTempVal.unshift(array[array.length - 1]);
-                array.length -= 1;
-                console.log(array.length)
             }
-        }
-        
-        if(arrayTempVal.length) {
-            push(array, arrayTempVal[0]);
+            // 2-ой вариант, когда что-то удаляем, но ничего не вставляем
+            if(deleteCount > 0 && numOfIncertElem === 0) {
+
+                if(i < deleteCount) {
+                    // элементы уходят в массив удаленных значений
+                    arrayDeletedVal[arrayDeletedVal.length] = array[i + start];
+                } else {
+                    // Элементы уходят в массив значений для сдвига 
+                    arrayTempVal[arrayTempVal.length] = array[i + start];
+                }
+            }
+            // 3-ий вариант, когда что-то удаляем и что-то всовываем
+            if(deleteCount > 0 && numOfIncertElem > 0) {
+                
+                if(i < deleteCount) {
+                    // элементы уходят в массив удаленных значений
+                    arrayDeletedVal[arrayDeletedVal.length] = array[i + start];
+                } else {
+                    
+                    if(array[i + start]) {
+                        // Элементы уходят в массив значений для сдвига
+                        arrayTempVal[arrayTempVal.length] = array[i + start];
+                    }
+                } 
+                if(arguments[i + startIncertElemInArg]) {
+                    // всовываем элемент 
+                    array[i + start] = arguments[i + startIncertElemInArg];
+                }
+            }
+
+        // пока массив с временными значениями не пустой    
+        } else if(arrayTempVal.length) {
+
+            if(deleteCount > numOfIncertElem) {
+                array[start + numOfIncertElem + i - numberOfIterations] = arrayTempVal[0];
+            } else {
+                // если кол-во вставляемых эл-тов менее кол-ва сдвигаемых элементов(кол-во итераций - кол-во сдвигаемых элементов)
+                if(numOfIncertElem < arrayTempVal.length ) {
+                    // тут перетираем значения начиная с индекса (start + numOfIncertElem), (i - numberOfIterations) - увеличивает счетчик
+                    array[start + numOfIncertElem + i - numberOfIterations] = arrayTempVal[0];
+                } else {
+                    push(array, arrayTempVal[0]);   
+                }
+            }
+            
             arrayTempVal.shift();
         }
+    }
+    // условие для 2-го варианта, где длинна массива остается без учета удаленных элементов
+    if(numOfIncertElem === 0) {
+        array.length = start + numberOfIterations - deleteCount;            
+    } 
+    // условие для 3-го варианта, где длинна массива остается без учета удаленных элементов
+    if(deleteCount > numOfIncertElem) {
+        array.length = originlArraySize - deleteCount + numOfIncertElem;
     }
     
     return arrayDeletedVal;
@@ -96,7 +138,10 @@ function push(array, element) {
 }
 
 let arr = [1, 3, 4, 6, 9, 11];
-let spl = splice(arr, 2, 2, "num") ;
+let spl = splice(arr, 2, 2, "ghj", "tyu", "yui");
 
 console.log(spl, "work function");
 console.log(arr);
+
+// "ghj", "tyu", "yui", "qwe", "tyui"
+// 
