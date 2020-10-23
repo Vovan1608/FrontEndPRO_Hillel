531fe7aa9515c737a5517067392f1419cba42465
Vovan1608 <vovan1608@gmail.com>
2020-10-23 22:09:31 +0300

    added 10.1

---
+++ #10_This/10.1/main.js
@@ -0,0 +1,20 @@
+﻿// "use strict";
+
+/*
+Задан обьект с любым количеством свойств. Одно из свойств является функция renderObject(), которая описана в window. При вызове метода obj.renderObject() -> выводит в document всё содержимое объекта, кроме самого метода renderObject
+
+    obj => { x:10, y: 20 }
+    obj.renderObject() -> x:10, y: 20
+*/
+
+var obj = {
+  a: 12,
+  rt: 5
+}
+
+obj.renderObject = function () {
+  
+  console.log(Object.values(obj));
+}
+
+obj.renderObject(); 
