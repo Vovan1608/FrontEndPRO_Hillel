6cc4011d544fbc0c2bd23e1eb120ee0d37b52770
Vovan1608 <vovan1608@gmail.com>
2020-10-24 08:52:54 +0300

    solved 11.2

---
+++ #11_This/11.2/main.js
@@ -0,0 +1,27 @@
+﻿// "use strict";
+
+/*
+Реализовать объект с методами m1(), m2(), m3(). Должна быть возможность выполнять подобный код:
+
+obj.m1().m2().m3();
+obj.m2().m1().m3();
+obj.m2().m1().m3().m1().m1();
+...
+*/
+
+var obj = {
+  m1: function() {
+    console.log("m1");
+    return this; // нужно возвращать объект в каждом методе(this это копия obj)
+  },
+  m2: function() {
+    console.log("m2");
+    return this;
+  },
+  m3: function() {
+    console.log("m3")
+    return this;
+  }
+}
+
+obj.m1().m2().m3().m1();
