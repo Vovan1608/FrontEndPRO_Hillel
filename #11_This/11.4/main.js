be6bfe2673705d81d2128983557c031d7b9840bf
Vovan1608 <vovan1608@gmail.com>
2020-10-26 09:18:23 +0200

    added flag in method, arguments

--- #11_This/11.4/main.js
+++ #11_This/11.4/main.js
@@ -1,26 +1,30 @@
 ﻿// "use strict";
 
 /*
-в обьекте data существует метод addRecord, который аргументами получает любой набор объектов. Метод addRecord добавляет все свойства переданных объектов в data.
+в обьекте data существует метод addRecord, который аргументами получает любой набор объектов. 
+Метод addRecord добавляет все свойства переданных объектов в data.
 
-В метод addRecord добавляется последний необязательный аргумент flag, который указывает приоритет присвоения свойств с одинаковым названием. Если true - берется свойство из первоначального объекта this, если false - берется свойство из arguments. По умолчанию flag = false;
+В метод addRecord добавляется последний необязательный аргумент flag, который указывает приоритет 
+присвоения свойств с одинаковым названием. Если true - берется свойство из первоначального объекта 
+this, если false - берется свойство из arguments. По умолчанию flag = false;
 */
 
 var data = {
-  addRecord: function(...arg) {
+  addRecord: function(flag = false) {
     var i = 0,
-        size = (typeof arg[arg.length - 1] === "object") ? arg.length : arg.length - 1; 
+        size = (typeof arguments[arguments.length - 1] === "object") ? arguments.length : arguments.length - 1;
   
     for(i; i < size; i += 1) {
 
-      if(size === arg.length) {
+      if(size === arguments.length) {
         Object.assign(this, arg[i]);
       } else {
-        for(var key in arg[i]){
+        
+        for(var key in arguments[i]) {
           flag = (key in this) ? true : false;
           
           if(!flag) {
-            this[key] = arg[i][key];
+            this[key] = arguments[i][key];
           }
         }
       }
@@ -31,7 +35,9 @@ var data = {
   y: -50
 };
 
-data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50}, flag = false);
+var flag;
+
+data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50}, flag);
 data.x // 10
 data.y // -50
 data.z // 30
