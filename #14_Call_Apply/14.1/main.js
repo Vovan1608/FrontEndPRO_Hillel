e9e144358f1e4687142a3ba6b855b7e5964c911e
Vovan1608 <vovan1608@gmail.com>
2020-10-29 08:45:10 +0200

    added message in 25 str

--- #14_Call_Apply/14.1/main.js
+++ #14_Call_Apply/14.1/main.js
@@ -1,5 +1,26 @@
 ﻿// "use strict";
 
 /*
-
+Пусть есть объект 
+obj = {
+  anonim: {say: 'Hello anonim'}, 
+  user: {say: 'Hello user'}, 
+  admin: {say: 'Hello admin'}
+}, 
+и ф-я: function greeting(who) {}
+при помощи метода call вызвать ф-ю greeting как метод объекта obj без записи в него, 
+с одним из 3ьох значений "anonim, admin, user". Получить в ответ сообщение приветствия.
 */
+
+var obj = {
+  anonim: {say: 'Hello anonim'}, 
+  user: {say: 'Hello user'}, 
+  admin: {say: 'Hello admin'}
+};
+
+function greeting(who) {
+  return who.say;
+}
+
+var message = greeting.call(obj, obj.admin);
+console.log(message);
