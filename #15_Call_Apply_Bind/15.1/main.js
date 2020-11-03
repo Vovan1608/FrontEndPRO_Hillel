ae02fc282e27193300241ad15bf365e204fbceed
Vovan1608 <vovan1608@gmail.com>
2020-11-03 20:55:21 +0200

    added 15.1

---
+++ #15_Call_Apply_Bind/15.1/main.js
@@ -0,0 +1,17 @@
+﻿// "use strict";
+
+/*
+Cоздать глобальную ф-ю greeting которая каждые 5000ms смотрит в тег 
+input с id='name' (получить при помощи document.querySelector('input[id="name"]')) берет значение по ключу value и выводит сообщение преветствия из 
+глобальной переменной greetingMessage.
+alеrt(greetingMessage + input.value)
+*/
+
+var greetingMessage = "Hello",
+    person = document.querySelector('input[id="name"]');
+
+function greeting(greetingMessage) {
+  alert(greetingMessage + ", " + person.value);
+}
+
+setInterval(greeting.bind(person, greetingMessage), 5000);
