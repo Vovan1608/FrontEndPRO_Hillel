410cf5e9146d43ac362370411bd8f6d504585c8c
Vovan1608 <vovan1608@gmail.com>
2020-11-08 14:45:29 +0200

    added 17

--- #17_OOP/16.1/main.js
+++ #17_OOP/16.1/main.js
@@ -1,94 +1,51 @@
 ﻿// "use strict";
 
 /*
-1.  Создать класс SuperMath. 
-2.  Добавить к экземпляру метод - check(obj), параметр obj которого 
-    имеет свойства X, Y, znak. 
-3.  Метод должен подтвердить у пользователя хочет ли он произвести 
-    действие znak c Х и У. 
-  3.1. Если - да, сделать математическое действие znak(которое описано в прототипе), 
-  3.2. иначе - запросить ввод новых данных через метод input() класса SuperMath. 
+Создать игру:
+Есть поле field, является экземпляром класса Field(width, height), 
+предпочитаемые размеры 10 на 10.
 
-Пример обекта: obj = { X:12, Y:3, znak: “/”}, возможные 
-варианты znak=> + - / * %. 
-При вводе znak нужно сделать проверку корректности ввода на возможные 
-математические действия
+Есть герой person, класс Person(name, XPosition, YPosition)
 
-p = new SuperMath();
-p.check(obj); // --> no p.input() -> 3 prompt -> считает
+Поле являет собой двумерный массив, который состоит из нулей. 
+Там где находится наш герой - стоит 1.
+
+Field имеет такие методы: 
+    * renderField() - выводит поле в document, устанавливает 
+      разделители `<hr />` сверху и снизу
+    * clearField() - убирает героя с доски
+    * changeSize(newX, newY) - меняет размер поля
+
+Person имеет такие методы:
+    * start() - герой появляется на доске
+    * go(direction, step) - движение по переданом параметру 
+      (direction может быть: 'left', 'right', 'top', 'bottom') и 
+      с шагом step
+    * resetPosition() - перенос в начальную позицию
+Нужно самим решить какие методы получают аргументы, какие нет. 
+Организовывать код можно любым образом.
+
+Вызов методов происходит из консоли.
+
+Суть такая:
+
+field.renderField();
+person.start();
+person.go('left', 2);
+person.go('top', 1);
+field.renderField();
+последовательно вызываются команды, renderField - отображает новую 
+доску в document. В результате мы должны увидеть 2 нарисованных 
+состояния поля.
 */
 
-// Создать класс SuperMath.
-function SuperMath() {
-  
+function Field(width, height) {
+  this.width = width;
+  this.height = height;
 }
 
-// метод input() класса SuperMath
-SuperMath.prototype.input = function() {
-  var newObj = {
-    x: Number(prompt("Введите Х", "12")),
-    y: Number(prompt("Введите Y", "3")),
-    znak: prompt("Введите znak", "/")
-  }
-  return this.doMath(newObj);
+function Person(name, XPosition, YPosition) {
+  this.name = name;
+  this.XPosition = XPosition;
+  this.YPosition = YPosition;
 }
-
-//сделать математическое действие znak(которое описано в прототипе)
-SuperMath.prototype.doMath = function(obj) {
-  // деструктуризация объекта
-  var {x, y, znak} = obj;
-
-  if(isNumber(x) && isNumber(y) && isZnak(znak) ) {
-
-    switch(znak) {
-      case "+":
-        return x + y;
-      case "-": 
-        return x - y;
-      case "*":
-        return x * y;
-      case "/":
-        if( y !== 0) {
-          return x / y;
-        } else {
-          throw new Error("incorrect value y");
-        }
-      case "%":
-        return x % y;
-    }
-  } else {
-    throw new Error("incorrect value");
-  }
-}
-
-// вспомогательные функции для проверок
-function isNumber(val) {
-  return (isNaN(val) || val === "" || typeof val === "boolean" || typeof val === "string" || val === null) ? false : true;
-}
-
-function isZnak(znak) {
-  return (["+", "-", "*", "/", "%"].includes(znak)) ? true : false;
-}
-
-function isObject(obj) {
-  return (typeof obj === "object" || obj !== null || !Array.isArray(obj)) ? true : false;
-}
-
-var p = new SuperMath();
-
-// Добавить к экземпляру метод check(obj)
-p.check = function(obj) {
-  // проверка на object
-  if(isObject(obj)) {
-    // подтвердить у пользователя хочет ли он произвести действие znak c Х и У.
-    return confirm(`Хотите ли Вы произвести действие  "${obj.znak}" c ${obj.x} и ${obj.y}?`) ? this.doMath(obj) : this.input();
-  }
-}
-
-var obj = {
-  x: 12,
-  y: 3,
-  znak: "-"
-};
-
-console.log(p.check(obj));
