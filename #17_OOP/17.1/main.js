c7d24b10abb9859be84650f2d7d5fff6f11f8b4b
Vovan1608 <vovan1608@gmail.com>
2020-11-17 21:35:24 +0200

    changed apply to call

--- #17_OOP/17.1/main.js
+++ #17_OOP/17.1/main.js
@@ -7,22 +7,22 @@
 
 Есть герой person, класс Person(name, XPosition, YPosition)
 
-Поле являет собой двумерный массив, который состоит из нулей. 
+Поле являет собой двумерный массив, который состоит из нулей.
 Там где находится наш герой - стоит 1.
 
-Field имеет такие методы: 
-    * renderField() - выводит поле в document, устанавливает 
+Field имеет такие методы:
+    * renderField() - выводит поле в document, устанавливает
       разделители `<hr />` сверху и снизу
     * clearField() - убирает героя с доски
     * changeSize(newX, newY) - меняет размер поля
 
 Person имеет такие методы:
     * start() - герой появляется на доске
-    * go(direction, step) - движение по переданом параметру 
-      (direction может быть: 'left', 'right', 'top', 'bottom') и 
+    * go(direction, step) - движение по переданом параметру
+      (direction может быть: 'left', 'right', 'top', 'bottom') и
       с шагом step
     * resetPosition() - перенос в начальную позицию
-Нужно самим решить какие методы получают аргументы, какие нет. 
+Нужно самим решить какие методы получают аргументы, какие нет.
 Организовывать код можно любым образом.
 
 Вызов методов происходит из консоли.
@@ -34,40 +34,44 @@ person.start();
 person.go('left', 2);
 person.go('top', 1);
 field.renderField();
-последовательно вызываются команды, renderField - отображает новую 
-доску в document. В результате мы должны увидеть 2 нарисованных 
+последовательно вызываются команды, renderField - отображает новую
+доску в document. В результате мы должны увидеть 2 нарисованных
 состояния поля.
 */
 
-function Field(width, height) {
-  // проверка на правильные значения
-  if( isPositiveInteger(width) && isPositiveInteger(height) ){
+// возвращает объект с заданной шириной и высотой (будующие размеры поля)
+function Field(width = 10, height = 10) {
+  if (isPositiveInteger(width) && isPositiveInteger(height)) {
     this._width = width;
     this._height = height;
   }
 }
-
-Field.prototype.renderField = function(width, height) {
+// рисует поле(принимает два параметра - ширину и высоту поля, в
+// document.write отрисовывает поле, ничего не возвращает)
+// логика должна быть следующая:
+//  - если в функцию параметры не передали, то параметры беруться из объекта new Field()
+//  - если параметры передали, то поле строится по указанным параметрам
+Field.prototype.renderField = function (...args) {
   document.write("<hr/>");
-  // если передано два аргумента и они положительные целые, то это и есть ширина и высота(для метода changeSize())
-  if(arguments.length === 2 && isPositiveInteger(width) && isPositiveInteger(height) ) {
-    width = width;
-    height = height;
+  // если передано два аргумента (для метода changeSize())
+  if (args.length === 2 && isPositiveInteger(args[0]) && isPositiveInteger(args[1])) {
+    width = args[0];
+    height = args[1];
   } else {
-    if(Array.isArray(arguments[0])) { // если переданный аргумент массив, значит рисуем героя по координатам
-      var x = arguments[0][1],
-          y = arguments[0][0];
+    if (Array.isArray(args[0])) { // если переданный аргумент массив, значит рисуем героя по координатам
+      var x = args[0][1],
+        y = args[0][0];
     }
-    // берем переданные в конструктор значения
+    // из объекта new Field()
     width = this._width;
     height = this._height;
   }
 
-  for(var i = 0; i < height; i += 1) {
+  for (var i = 0; i < height; i += 1) {
 
-    for(var j = 0; j < width; j += 1) {
+    for (var j = 0; j < width; j += 1) {
 
-      if(i === x && j === y) {
+      if (i === x && j === y) {
         document.writeln("1");
       } else {
         document.writeln("0");
@@ -77,46 +81,45 @@ Field.prototype.renderField = function(width, height) {
   }
   document.write("<hr/>");
 }
-
-Field.prototype.clearField = function() {
-  // пустое поле с размерами из конструктора, т.к. ничего не передаем
+// функция вызывает метод renderField() без параметров (чистое поле)
+Field.prototype.clearField = function () {
   this.renderField();
 }
-
-Field.prototype.changeSize = function(newX, newY) {
-  // поле с этими размерами
+// функция вызывает метод renderField() с двумя пар-ми пр которым рисуется новое поле
+Field.prototype.changeSize = function (newX, newY) {
   this.renderField(newX, newY);
 }
-// добавил width, height из конструктора родителя
-function Person(width, height, name, XPosition, YPosition) {
+// возвращает объект new Person
+function Person(name, XPosition, YPosition) {
   // наследование св-в родителя
-  Field.apply(this, arguments);
+  Field.call(this);
   this.name = name;
   // проверка,что герой не выпадает из пределов поля
-  if(XPosition <= this._width) {
+  if (XPosition <= this._width) {
     this.XPosition = XPosition;
   }
 
-  if(YPosition <= this._height) {
+  if (YPosition <= this._height) {
     this.YPosition = YPosition;
   }
 }
 //наследуюсь от класса Field
 Person.prototype = Object.create(Field.prototype);
 Person.prototype.constructor = Person;
-
-Person.prototype.start = function() {
-  //возвр. массив начальных координат
+// функция возвращает массив стартовых координат, которые берутся из new Person()
+// (нужны для отрисовки героя на доске)
+Person.prototype.start = function () {
   return [this.XPosition, this.YPosition];
 }
-
-Person.prototype.go = function(direction, step) {
-  if(direction) {
+// функция принимает два параметра(стринг и число) и возвращает
+// массив координат, куда переместился герой
+Person.prototype.go = function (direction, step) {
+  if (direction) {
     // массив координат старта
     var coord = this.start(),
-        x = coord[0],
-        y = coord[1];
-    switch(direction) {
+      x = coord[0],
+      y = coord[1];
+    switch (direction) {
       case "right":
         x = (x + step > this._width) ? x : x + step;
         return [x, y];
@@ -132,9 +135,8 @@ Person.prototype.go = function(direction, step) {
     }
   }
 }
-
-Person.prototype.resetPosition = function() {
-  // для сброса вызыв метод старт, где массив начальных координат
+// функция устанавливает стартовые координаты
+Person.prototype.resetPosition = function () {
   return this.start();
 }
 // Вспом. функции
@@ -143,8 +145,8 @@ function isDirecton(direction) {
 }
 
 function isPositiveInteger(num) {
-  if(isNumber(num)) {
-    return (num > 0 && Number.isInteger(num) ) ? true : false;
+  if (isNumber(num)) {
+    return (num > 0 && Number.isInteger(num)) ? true : false;
   }
 }
 
@@ -154,7 +156,7 @@ function isNumber(val) {
 
 var field = new Field(10, 10);
 
-var person = new Person(10, 10, "Bob", 5, 3);
+var person = new Person("Bob", 5, 3);
 field.renderField()
 field.renderField(person.start());
 field.renderField(person.go("top", 2))
