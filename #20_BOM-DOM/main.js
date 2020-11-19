813ba85957fc827e27a4736941c25e5f5186a558
Vovan1608 <vovan1608@gmail.com>
2020-11-19 16:59:02 +0200

    added 20

---
+++ #20_BOM-DOM/main.js
@@ -0,0 +1,18 @@
+﻿// "use strict";
+
+/*
+Есть 3 input. Выводить в textarea содержимое всех
+полей ввода через запятую. Использовать setInterval.
+*/
+
+setInterval( () => {
+	let inputs = document.getElementsByTagName('input'),// коллекция input
+		  textArea = document.getElementById('text'),
+			arrInputs = [],//массив для значений input
+			i = 0,
+			size = inputs.length;
+	for(i; i < size; i += 1) {
+		arrInputs.push(inputs[i].value);
+	}
+	textArea.innerText = arrInputs.join(", ")
+}, 3000);
