cc2dd45161ec74bf17b9f477658b22e8bf8b14ab
Vladymyr Zenin <vovan1608@gmail.com>
2020-12-06 18:49:43 +0200

    changed all logic

--- #23_DOM(phase) + Сreate element/main.js
+++ #23_DOM(phase) + Сreate element/main.js
@@ -12,63 +12,64 @@ P.S. обязательно использовать делегирование 
 window.onload = function() {
 
 	const buttons = [{button: "SAVE"}, {button: "CANCEL"}];
-	
+
 	const getElement = (selector) => document.querySelector(selector);
 
 	const createElem = (selector) => document.createElement(selector);
 
 	const createButtons = (buttons) => {
-		const saveCancelButton = [];
+		const containersButton = [];
 		buttons.forEach(item => {
 			const buttn = createElem("button");
 			buttn.innerText = item.button;
-			saveCancelButton.push(buttn);
+			containersButton.push(buttn);
 		});
-		return saveCancelButton;
+		return containersButton;
 	}
 
-	const handler = (event) => {
-		// элемент по которому кликнули
-		const clickedElem = event.target;
-		// текст элемента по которому кликнули
-		const textClickedElement = clickedElem.innerText;
-
-		if(clickedElem.tagName === "TD") {
-			// общий контейнер для вставки
-			const container = createElem("div");
-			// container.classList.add("container");
-			
-			// элемент текстовое поле
-			const textarea = createElem("textarea");
-			// содержание текстового поля как у элемента, на который кликнули
-			textarea.innerText = textClickedElement;
-
-			// блок с кнопками
-			const [save, cancel] = createButtons(buttons);
-
-			// общий контейнер 
-			container.append(textarea, save, cancel);
-
-			// контейнер вставляется в элемент по которому кликнули
-			clickedElem.append(container);
-			// добавить контейнеру класс и перезаписать все классы у него на один
-			container.className  = "container";
-
-			save.addEventListener("click", () => {
-				clickedElem.innerText = textarea.value;
-				// контейнеру добавляется класс hidden - скрыть
-				container.classList.add("hidden");
-			});
-			cancel.addEventListener("click", () => {
-				clickedElem.innerText = textClickedElement;
-				container.classList.add("hidden");
-			});
-		}
+	const createElemOfContainer = () => {
+		// элемент текстовое поле
+		const textarea = createElem("textarea");
+		// блок с кнопками
+		const [save, cancel] = createButtons(buttons);
+		// общий контейнер для вставки
+		const container = createElem("div");
+		return {container, textarea, save, cancel};
 	}
 
 	const onListenerTable = () => {
 		const table = getElement("table");
-		table.addEventListener("click", handler);
+		table.addEventListener("click", (event) => {
+
+			if(event.target.tagName === "TD") {
+				const {container, textarea, save, cancel} = createElemOfContainer();
+				// отрисовать в container элементы textarea, save, cancel
+				container.append(textarea, save, cancel);
+				// в кликнутом элементе вставить container
+				event.target.append(container);
+				// текст элемента по которому кликнули
+				const textClickedElement = event.target.firstChild.nodeValue;
+				// текстовое поле с текстом который был в ячейке(на которую нажали)
+				textarea.value = textClickedElement;
+				container.classList.add("show");
+
+				document.querySelectorAll("button").forEach(item => {
+					if(item.tagName === "BUTTON") {
+						item.addEventListener("click", () => {
+							if(item.innerText === "SAVE") {
+								event.target.innerText = textarea.value;
+								// удаляем класс show
+								container.classList.remove("show");
+							}
+							if(item.innerText === "CANCEL") {
+								event.target.innerText = textClickedElement;
+								container.classList.remove("show");
+							}
+						});
+					}
+				});
+			}
+		});
 	}
 
 	onListenerTable();
