7cb4c4dbce19e37b6f615a42d18da21ed4aaacc6
Vovan1608 <vovan1608@gmail.com>
2020-12-07 19:09:07 +0200

    added createElementForBlock(), createBlock()

--- #25_Storages + JSON/main.js
+++ #25_Storages + JSON/main.js
@@ -12,5 +12,32 @@ counter должен сохраняться.
 */
 
 window.onload = function() {
+	// функция создает контейнер с кнопками и возвр. объект
+	// с этими данными
+	const createElementForBlock = () => {
+		const container = document.createElement("div");
+		container.classList.add("container");
+		const button = document.createElement("button");
+		button.innerText = "click";
+		button.classList.add("btn");
+		const tablo = document.createElement("button");
+		tablo.innerText = 0;
+		tablo.classList.add("tablo");
+		return {container, button, tablo};
+	}
+	// функция создает блок с необходимым кол-вом контейнеров
+	const createBlock = () => {
+		const mainContainer = document.querySelector("#main");
+		const fragment = document.createDocumentFragment();
+		const numOfConteiners = 2;
+		for(let i = 0; i < numOfConteiners; i += 1) {
+			const {container, button, tablo} = createElementForBlock();
+			container.append(button, tablo);
+			fragment.append(container);
+		}
 
+		mainContainer.append(fragment);
+	}
+	
+	createBlock();
 }
