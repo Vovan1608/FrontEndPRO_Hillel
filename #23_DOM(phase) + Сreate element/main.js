1f00a9a7fe9270321b06dfdde8eed7068605105c
Vovan1608 <vovan1608@gmail.com>
2020-12-09 08:24:39 +0200

    done?

--- #23_DOM(phase) + Сreate element/main.js
+++ #23_DOM(phase) + Сreate element/main.js
@@ -28,7 +28,7 @@ window.onload = function() {
 		return containersButton;
 	}
 
-	const createElemOfContainer = () => {
+	const createElemContainer = () => {
 		// элемент текстовое поле
 		const textarea = createElem("textarea");
 		// блок с кнопками
@@ -38,17 +38,21 @@ window.onload = function() {
 		return {container, textarea, save, cancel};
 	}
 
-	const handler = (event) => {
-		const clickedElem = event.target;
-		console.log(clickedElem);
+	const getEvent = (event) => {
+		return event.target;
+	}
+
+	const handlerTD = (event) => {
+		const clickedElem = getEvent(event);
 
 		if(clickedElem.tagName === "TD") {
-			const {container, textarea, save, cancel} = createElemOfContainer();
+			const {container, textarea, save, cancel} = createElemContainer();
 			// отрисовать в container элементы textarea, save, cancel
 			container.append(textarea, save, cancel);
 			// в кликнутом элементе вставить container
 			clickedElem.append(container);
-
+			// кликая на container, рисуем его один раз (остальные клики не рисуем)
+			container.className = (clickedElem.childElementCount === 1) ? "show" : "hidden";
 			// текст элемента по которому кликнули
 			const textClickedElement = clickedElem.firstChild.nodeValue;
 			// текстовое поле с текстом который был в ячейке(на которую нажали)
@@ -56,10 +60,11 @@ window.onload = function() {
 
 			document.querySelectorAll("button").forEach(item => {
 				item.addEventListener("click", (e) => {
-					clickedElem.innerText = (e.target.getAttribute("data_name") === "save") ? 
+					// содержимое кликнутого элемента меняется в зависимости от нажатой кнопки
+					clickedElem.innerText = (getEvent(e).getAttribute("data_name") === "save") ? 
 						textarea.value : textClickedElement;
 					
-						container.className = "hidden";
+					container.className = "hidden";
 				});
 			});
 		}
@@ -67,7 +72,7 @@ window.onload = function() {
 
 	const onListenerTable = () => {
 		const table = getElement("table");
-		table.addEventListener("click", handler);
+		table.addEventListener("click", handlerTD);
 	}
 
 	onListenerTable();
