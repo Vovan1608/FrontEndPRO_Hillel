abe6e16507f717cbd665c7a49fe24d6553f18fb2
Vovan1608 <vovan1608@gmail.com>
2020-12-10 13:47:37 +0200

    added express

--- #26_NPM,HTTP, NodeJS(Express) starter/main.js
+++ #26_NPM,HTTP, NodeJS(Express) starter/main.js
@@ -3,3 +3,15 @@
 /*
 
 */
+
+var express = require("express"); // подключить express(упрощение для NodeJs) из папки node_modules
+var fs = require("fs"); // fs -- объект который дает возможность читать файлы(например json)
+var bodyParser = require("body-parser"); // 'body-parser' -- библиотека дает возможность прочитать post запрос на NodeJs
+var app = express();
+
+
+const port = 3003;
+
+app.listen(port, function() {
+  console.log(`Example app listening on port http://localhost:${port}/`);
+});
